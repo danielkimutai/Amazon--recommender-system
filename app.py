17340665af9e88726a38b09648a3f7bc3327ff54
“danielkimutai” <danielkimutaikorir@gmail.com>
2023-05-25 16:57:31 +0300

    Final

--- app.py
+++ app.py
@@ -1,37 +1,54 @@
 import pandas as pd
+import pandas as pd
 import streamlit as st
-from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-import numpy as np 
-import regex
-import re
-import string
-from wordcloud import WordCloud, STOPWORDS
-import matplotlib.pyplot as plt
-from sklearn.feature_extraction.text import CountVectorizer 
-from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Load data
+data = pd.read_csv("clean.csv")
+
+# Title and description
 st.title("Amazon Recommender System")
-st.write("This is a system that uses content based similarity to recommend products.The data mainly contains Amazon Products")
-if st.button('Show dataframe'):
-    data = pd.read_csv("clean.csv")        
-    data[:10]
-st.header("Most Expensive Products")
-data = pd.read_csv("clean.csv")  
-df_group10 = data.groupby("subcategory")["Price"].mean().sort_values(ascending = False)[:10]
-# Reset the index
-df_group10 = df_group10.reset_index()
-st.bar_chart(df_group10,x="subcategory",y="Price")
-def wordcloud(data,column):
-    text = ' '.join(data[column])
-    wordcloud = WordCloud(width = 800, height = 800,
-                    min_font_size = 10).generate(text)
-    # plot the WordCloud image                      
-    plt.figure(figsize = (8, 8), facecolor = None)
-    plt.imshow(wordcloud)
-    plt.axis("off")
-    plt.tight_layout(pad = 0)
-    plt.title(f'{column} word cloud')
-    st.pyplot()
-wordcloud(data,"Product")
-        
+st.write("This system uses content-based similarity to recommend products from Amazon.")
+
+# Show list of products button
+if st.button('List of Products'):
+    st.write(data[["Product", "Price", "Rating"]])
+
+# Get user input
+st.subheader("What would you like to purchase?")
+product_name = st.text_input("Enter the product name:")
+
+# Recommender function
+def recommender(product_name, data):
+    # Check if the product exists in the dataset
+    if product_name not in data["Product"].values:
+        st.write("Product not found. Please enter a valid product name.")
+        return
+
+    # Create TF-IDF matrix
+    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf.fit_transform(data["About"].fillna(""))
+
+    # Calculate cosine similarity
+    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+    # Get product index
+    product_index = data[data["Product"] == product_name].index[0]
+
+    # Calculate similarity scores
+    similarity_scores = pd.DataFrame(cosine_sim[product_index], columns=["score"])
+    product_indices = similarity_scores.sort_values("score", ascending=False).index[1:6]
+
+    # Return recommended products
+    recommended_products = data.loc[product_indices, ["Product", "Price", "Rating"]]
+    return recommended_products
+
+# Display recommended products
+if product_name:
+    recommended = recommender(product_name, data)
+    if recommended is not None:
+        st.write("You may also like:")
+        st.table(recommended)
+
